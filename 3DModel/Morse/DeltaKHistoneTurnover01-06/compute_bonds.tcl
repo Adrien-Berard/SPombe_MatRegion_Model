c7fcd5e18d0f107f502244f0535ed2bdb6561de3
AdrienBerard <berardadrien7@gmail.com>
2024-06-02 13:28:59 +0200

    UpdatesTCL file

---
+++ 3DModel/Morse/DeltaKHistoneTurnover01-06/compute_bonds.tcl
@@ -0,0 +1,709 @@
+
+# Load the LAMMPS trajectory file
+mol new dump.lammpstrj type lammpstrj
+
+# Set bond computation parameters
+set mol [atomselect top all]
+$mol setbonds {}
+
+# Add bond between atom 1 and atom 2
+mol addbond 1 2
+
+# Add bond between atom 2 and atom 3
+mol addbond 2 3
+
+# Add bond between atom 3 and atom 4
+mol addbond 3 4
+
+# Add bond between atom 4 and atom 5
+mol addbond 4 5
+
+# Add bond between atom 5 and atom 6
+mol addbond 5 6
+
+# Add bond between atom 6 and atom 7
+mol addbond 6 7
+
+# Add bond between atom 7 and atom 8
+mol addbond 7 8
+
+# Add bond between atom 8 and atom 9
+mol addbond 8 9
+
+# Add bond between atom 9 and atom 10
+mol addbond 9 10
+
+# Add bond between atom 10 and atom 11
+mol addbond 10 11
+
+# Add bond between atom 11 and atom 12
+mol addbond 11 12
+
+# Add bond between atom 12 and atom 13
+mol addbond 12 13
+
+# Add bond between atom 13 and atom 14
+mol addbond 13 14
+
+# Add bond between atom 14 and atom 15
+mol addbond 14 15
+
+# Add bond between atom 15 and atom 16
+mol addbond 15 16
+
+# Add bond between atom 16 and atom 17
+mol addbond 16 17
+
+# Add bond between atom 17 and atom 18
+mol addbond 17 18
+
+# Add bond between atom 18 and atom 19
+mol addbond 18 19
+
+# Add bond between atom 19 and atom 20
+mol addbond 19 20
+
+# Add bond between atom 20 and atom 21
+mol addbond 20 21
+
+# Add bond between atom 21 and atom 22
+mol addbond 21 22
+
+# Add bond between atom 22 and atom 23
+mol addbond 22 23
+
+# Add bond between atom 23 and atom 24
+mol addbond 23 24
+
+# Add bond between atom 24 and atom 25
+mol addbond 24 25
+
+# Add bond between atom 25 and atom 26
+mol addbond 25 26
+
+# Add bond between atom 26 and atom 27
+mol addbond 26 27
+
+# Add bond between atom 27 and atom 28
+mol addbond 27 28
+
+# Add bond between atom 28 and atom 29
+mol addbond 28 29
+
+# Add bond between atom 29 and atom 30
+mol addbond 29 30
+
+# Add bond between atom 30 and atom 31
+mol addbond 30 31
+
+# Add bond between atom 31 and atom 32
+mol addbond 31 32
+
+# Add bond between atom 32 and atom 33
+mol addbond 32 33
+
+# Add bond between atom 33 and atom 34
+mol addbond 33 34
+
+# Add bond between atom 34 and atom 35
+mol addbond 34 35
+
+# Add bond between atom 35 and atom 36
+mol addbond 35 36
+
+# Add bond between atom 36 and atom 37
+mol addbond 36 37
+
+# Add bond between atom 37 and atom 38
+mol addbond 37 38
+
+# Add bond between atom 38 and atom 39
+mol addbond 38 39
+
+# Add bond between atom 39 and atom 40
+mol addbond 39 40
+
+# Add bond between atom 40 and atom 41
+mol addbond 40 41
+
+# Add bond between atom 41 and atom 42
+mol addbond 41 42
+
+# Add bond between atom 42 and atom 43
+mol addbond 42 43
+
+# Add bond between atom 43 and atom 44
+mol addbond 43 44
+
+# Add bond between atom 44 and atom 45
+mol addbond 44 45
+
+# Add bond between atom 45 and atom 46
+mol addbond 45 46
+
+# Add bond between atom 46 and atom 47
+mol addbond 46 47
+
+# Add bond between atom 47 and atom 48
+mol addbond 47 48
+
+# Add bond between atom 48 and atom 49
+mol addbond 48 49
+
+# Add bond between atom 49 and atom 50
+mol addbond 49 50
+
+# Add bond between atom 50 and atom 51
+mol addbond 50 51
+
+# Add bond between atom 51 and atom 52
+mol addbond 51 52
+
+# Add bond between atom 52 and atom 53
+mol addbond 52 53
+
+# Add bond between atom 53 and atom 54
+mol addbond 53 54
+
+# Add bond between atom 54 and atom 55
+mol addbond 54 55
+
+# Add bond between atom 55 and atom 56
+mol addbond 55 56
+
+# Add bond between atom 56 and atom 57
+mol addbond 56 57
+
+# Add bond between atom 57 and atom 58
+mol addbond 57 58
+
+# Add bond between atom 58 and atom 59
+mol addbond 58 59
+
+# Add bond between atom 59 and atom 60
+mol addbond 59 60
+
+# Add bond between atom 60 and atom 61
+mol addbond 60 61
+
+# Add bond between atom 61 and atom 62
+mol addbond 61 62
+
+# Add bond between atom 62 and atom 63
+mol addbond 62 63
+
+# Add bond between atom 63 and atom 64
+mol addbond 63 64
+
+# Add bond between atom 64 and atom 65
+mol addbond 64 65
+
+# Add bond between atom 65 and atom 66
+mol addbond 65 66
+
+# Add bond between atom 66 and atom 67
+mol addbond 66 67
+
+# Add bond between atom 67 and atom 68
+mol addbond 67 68
+
+# Add bond between atom 68 and atom 69
+mol addbond 68 69
+
+# Add bond between atom 69 and atom 70
+mol addbond 69 70
+
+# Add bond between atom 70 and atom 71
+mol addbond 70 71
+
+# Add bond between atom 71 and atom 72
+mol addbond 71 72
+
+# Add bond between atom 72 and atom 73
+mol addbond 72 73
+
+# Add bond between atom 73 and atom 74
+mol addbond 73 74
+
+# Add bond between atom 74 and atom 75
+mol addbond 74 75
+
+# Add bond between atom 75 and atom 76
+mol addbond 75 76
+
+# Add bond between atom 76 and atom 77
+mol addbond 76 77
+
+# Add bond between atom 77 and atom 78
+mol addbond 77 78
+
+# Add bond between atom 78 and atom 79
+mol addbond 78 79
+
+# Add bond between atom 79 and atom 80
+mol addbond 79 80
+
+# Add bond between atom 1 and atom 2
+mol addbond 1 2
+
+# Add bond between atom 2 and atom 3
+mol addbond 2 3
+
+# Add bond between atom 3 and atom 4
+mol addbond 3 4
+
+# Add bond between atom 4 and atom 5
+mol addbond 4 5
+
+# Add bond between atom 5 and atom 6
+mol addbond 5 6
+
+# Add bond between atom 6 and atom 7
+mol addbond 6 7
+
+# Add bond between atom 7 and atom 8
+mol addbond 7 8
+
+# Add bond between atom 8 and atom 9
+mol addbond 8 9
+
+# Add bond between atom 9 and atom 10
+mol addbond 9 10
+
+# Add bond between atom 10 and atom 11
+mol addbond 10 11
+
+# Add bond between atom 11 and atom 12
+mol addbond 11 12
+
+# Add bond between atom 12 and atom 13
+mol addbond 12 13
+
+# Add bond between atom 13 and atom 14
+mol addbond 13 14
+
+# Add bond between atom 14 and atom 15
+mol addbond 14 15
+
+# Add bond between atom 15 and atom 16
+mol addbond 15 16
+
+# Add bond between atom 16 and atom 17
+mol addbond 16 17
+
+# Add bond between atom 17 and atom 18
+mol addbond 17 18
+
+# Add bond between atom 18 and atom 19
+mol addbond 18 19
+
+# Add bond between atom 19 and atom 20
+mol addbond 19 20
+
+# Add bond between atom 20 and atom 21
+mol addbond 20 21
+
+# Add bond between atom 21 and atom 22
+mol addbond 21 22
+
+# Add bond between atom 22 and atom 23
+mol addbond 22 23
+
+# Add bond between atom 23 and atom 24
+mol addbond 23 24
+
+# Add bond between atom 24 and atom 25
+mol addbond 24 25
+
+# Add bond between atom 25 and atom 26
+mol addbond 25 26
+
+# Add bond between atom 26 and atom 27
+mol addbond 26 27
+
+# Add bond between atom 27 and atom 28
+mol addbond 27 28
+
+# Add bond between atom 28 and atom 29
+mol addbond 28 29
+
+# Add bond between atom 29 and atom 30
+mol addbond 29 30
+
+# Add bond between atom 30 and atom 31
+mol addbond 30 31
+
+# Add bond between atom 31 and atom 32
+mol addbond 31 32
+
+# Add bond between atom 32 and atom 33
+mol addbond 32 33
+
+# Add bond between atom 33 and atom 34
+mol addbond 33 34
+
+# Add bond between atom 34 and atom 35
+mol addbond 34 35
+
+# Add bond between atom 35 and atom 36
+mol addbond 35 36
+
+# Add bond between atom 36 and atom 37
+mol addbond 36 37
+
+# Add bond between atom 37 and atom 38
+mol addbond 37 38
+
+# Add bond between atom 38 and atom 39
+mol addbond 38 39
+
+# Add bond between atom 39 and atom 40
+mol addbond 39 40
+
+# Add bond between atom 40 and atom 41
+mol addbond 40 41
+
+# Add bond between atom 41 and atom 42
+mol addbond 41 42
+
+# Add bond between atom 42 and atom 43
+mol addbond 42 43
+
+# Add bond between atom 43 and atom 44
+mol addbond 43 44
+
+# Add bond between atom 44 and atom 45
+mol addbond 44 45
+
+# Add bond between atom 45 and atom 46
+mol addbond 45 46
+
+# Add bond between atom 46 and atom 47
+mol addbond 46 47
+
+# Add bond between atom 47 and atom 48
+mol addbond 47 48
+
+# Add bond between atom 48 and atom 49
+mol addbond 48 49
+
+# Add bond between atom 49 and atom 50
+mol addbond 49 50
+
+# Add bond between atom 50 and atom 51
+mol addbond 50 51
+
+# Add bond between atom 51 and atom 52
+mol addbond 51 52
+
+# Add bond between atom 52 and atom 53
+mol addbond 52 53
+
+# Add bond between atom 53 and atom 54
+mol addbond 53 54
+
+# Add bond between atom 54 and atom 55
+mol addbond 54 55
+
+# Add bond between atom 55 and atom 56
+mol addbond 55 56
+
+# Add bond between atom 56 and atom 57
+mol addbond 56 57
+
+# Add bond between atom 57 and atom 58
+mol addbond 57 58
+
+# Add bond between atom 58 and atom 59
+mol addbond 58 59
+
+# Add bond between atom 59 and atom 60
+mol addbond 59 60
+
+# Add bond between atom 60 and atom 61
+mol addbond 60 61
+
+# Add bond between atom 61 and atom 62
+mol addbond 61 62
+
+# Add bond between atom 62 and atom 63
+mol addbond 62 63
+
+# Add bond between atom 63 and atom 64
+mol addbond 63 64
+
+# Add bond between atom 64 and atom 65
+mol addbond 64 65
+
+# Add bond between atom 65 and atom 66
+mol addbond 65 66
+
+# Add bond between atom 66 and atom 67
+mol addbond 66 67
+
+# Add bond between atom 67 and atom 68
+mol addbond 67 68
+
+# Add bond between atom 68 and atom 69
+mol addbond 68 69
+
+# Add bond between atom 69 and atom 70
+mol addbond 69 70
+
+# Add bond between atom 70 and atom 71
+mol addbond 70 71
+
+# Add bond between atom 71 and atom 72
+mol addbond 71 72
+
+# Add bond between atom 72 and atom 73
+mol addbond 72 73
+
+# Add bond between atom 73 and atom 74
+mol addbond 73 74
+
+# Add bond between atom 74 and atom 75
+mol addbond 74 75
+
+# Add bond between atom 75 and atom 76
+mol addbond 75 76
+
+# Add bond between atom 76 and atom 77
+mol addbond 76 77
+
+# Add bond between atom 77 and atom 78
+mol addbond 77 78
+
+# Add bond between atom 78 and atom 79
+mol addbond 78 79
+
+# Add bond between atom 1 and atom 2
+mol addbond 1 2
+
+# Add bond between atom 2 and atom 3
+mol addbond 2 3
+
+# Add bond between atom 3 and atom 4
+mol addbond 3 4
+
+# Add bond between atom 4 and atom 5
+mol addbond 4 5
+
+# Add bond between atom 5 and atom 6
+mol addbond 5 6
+
+# Add bond between atom 6 and atom 7
+mol addbond 6 7
+
+# Add bond between atom 7 and atom 8
+mol addbond 7 8
+
+# Add bond between atom 8 and atom 9
+mol addbond 8 9
+
+# Add bond between atom 9 and atom 10
+mol addbond 9 10
+
+# Add bond between atom 10 and atom 11
+mol addbond 10 11
+
+# Add bond between atom 11 and atom 12
+mol addbond 11 12
+
+# Add bond between atom 12 and atom 13
+mol addbond 12 13
+
+# Add bond between atom 13 and atom 14
+mol addbond 13 14
+
+# Add bond between atom 14 and atom 15
+mol addbond 14 15
+
+# Add bond between atom 15 and atom 16
+mol addbond 15 16
+
+# Add bond between atom 16 and atom 17
+mol addbond 16 17
+
+# Add bond between atom 17 and atom 18
+mol addbond 17 18
+
+# Add bond between atom 18 and atom 19
+mol addbond 18 19
+
+# Add bond between atom 19 and atom 20
+mol addbond 19 20
+
+# Add bond between atom 20 and atom 21
+mol addbond 20 21
+
+# Add bond between atom 21 and atom 22
+mol addbond 21 22
+
+# Add bond between atom 22 and atom 23
+mol addbond 22 23
+
+# Add bond between atom 23 and atom 24
+mol addbond 23 24
+
+# Add bond between atom 24 and atom 25
+mol addbond 24 25
+
+# Add bond between atom 25 and atom 26
+mol addbond 25 26
+
+# Add bond between atom 26 and atom 27
+mol addbond 26 27
+
+# Add bond between atom 27 and atom 28
+mol addbond 27 28
+
+# Add bond between atom 28 and atom 29
+mol addbond 28 29
+
+# Add bond between atom 29 and atom 30
+mol addbond 29 30
+
+# Add bond between atom 30 and atom 31
+mol addbond 30 31
+
+# Add bond between atom 31 and atom 32
+mol addbond 31 32
+
+# Add bond between atom 32 and atom 33
+mol addbond 32 33
+
+# Add bond between atom 33 and atom 34
+mol addbond 33 34
+
+# Add bond between atom 34 and atom 35
+mol addbond 34 35
+
+# Add bond between atom 35 and atom 36
+mol addbond 35 36
+
+# Add bond between atom 36 and atom 37
+mol addbond 36 37
+
+# Add bond between atom 37 and atom 38
+mol addbond 37 38
+
+# Add bond between atom 38 and atom 39
+mol addbond 38 39
+
+# Add bond between atom 39 and atom 40
+mol addbond 39 40
+
+# Add bond between atom 40 and atom 41
+mol addbond 40 41
+
+# Add bond between atom 41 and atom 42
+mol addbond 41 42
+
+# Add bond between atom 42 and atom 43
+mol addbond 42 43
+
+# Add bond between atom 43 and atom 44
+mol addbond 43 44
+
+# Add bond between atom 44 and atom 45
+mol addbond 44 45
+
+# Add bond between atom 45 and atom 46
+mol addbond 45 46
+
+# Add bond between atom 46 and atom 47
+mol addbond 46 47
+
+# Add bond between atom 47 and atom 48
+mol addbond 47 48
+
+# Add bond between atom 48 and atom 49
+mol addbond 48 49
+
+# Add bond between atom 49 and atom 50
+mol addbond 49 50
+
+# Add bond between atom 50 and atom 51
+mol addbond 50 51
+
+# Add bond between atom 51 and atom 52
+mol addbond 51 52
+
+# Add bond between atom 52 and atom 53
+mol addbond 52 53
+
+# Add bond between atom 53 and atom 54
+mol addbond 53 54
+
+# Add bond between atom 54 and atom 55
+mol addbond 54 55
+
+# Add bond between atom 55 and atom 56
+mol addbond 55 56
+
+# Add bond between atom 56 and atom 57
+mol addbond 56 57
+
+# Add bond between atom 57 and atom 58
+mol addbond 57 58
+
+# Add bond between atom 58 and atom 59
+mol addbond 58 59
+
+# Add bond between atom 59 and atom 60
+mol addbond 59 60
+
+# Add bond between atom 60 and atom 61
+mol addbond 60 61
+
+# Add bond between atom 61 and atom 62
+mol addbond 61 62
+
+# Add bond between atom 62 and atom 63
+mol addbond 62 63
+
+# Add bond between atom 63 and atom 64
+mol addbond 63 64
+
+# Add bond between atom 64 and atom 65
+mol addbond 64 65
+
+# Add bond between atom 65 and atom 66
+mol addbond 65 66
+
+# Add bond between atom 66 and atom 67
+mol addbond 66 67
+
+# Add bond between atom 67 and atom 68
+mol addbond 67 68
+
+# Add bond between atom 68 and atom 69
+mol addbond 68 69
+
+# Add bond between atom 69 and atom 70
+mol addbond 69 70
+
+# Add bond between atom 70 and atom 71
+mol addbond 70 71
+
+# Add bond between atom 71 and atom 72
+mol addbond 71 72
+
+# Add bond between atom 72 and atom 73
+mol addbond 72 73
+
+# Add bond between atom 73 and atom 74
+mol addbond 73 74
+
+# Add bond between atom 74 and atom 75
+mol addbond 74 75
+
+# Add bond between atom 75 and atom 76
+mol addbond 75 76
+
+# Add bond between atom 76 and atom 77
+mol addbond 76 77
+
+# Add bond between atom 77 and atom 78
+mol addbond 77 78
